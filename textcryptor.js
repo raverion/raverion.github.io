194d771a362dfdf01a4cc08e2cac6eb1b7155254
Rej Averion <averionrej@gmail.com>
2023-02-15 23:39:29 +0800

    add save file functionality

--- textcryptor.js
+++ textcryptor.js
@@ -32,6 +32,20 @@ function openFile() {
   });
 
   fileInput.click();
+  delete fileInput;
 }
 
-function saveFile() {}
+function saveFile() {
+  if ((promptFilename = prompt("Save file as", ""))) {
+    var textBlob = new Blob([document.getElementById("textarea1").value], {
+      type: "text/plain",
+    });
+    var downloadLink = document.createElement("a");
+    downloadLink.download = promptFilename + ".txt";
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = window.URL.createObjectURL(textBlob);
+    downloadLink.click();
+    delete downloadLink;
+    delete textBlob;
+  }
+}

8e41aaac542de9aba5600f3065caedc72b9ad59d
Rej Averion <averionrej@gmail.com>
2023-02-15 16:06:52 +0800

    add open file functionality

--- textcryptor.js
+++ textcryptor.js
@@ -13,3 +13,25 @@ function decryptText() {
   const decrypted = decryptedText.toString(CryptoJS.enc.Utf8);
   document.getElementById("textarea1").value = decrypted;
 }
+
+function openFile() {
+  let fileInput = document.createElement("input");
+  fileInput.type = "file";
+  fileInput.accept = ".txt";
+
+  let fileContents;
+  fileInput.addEventListener("change", function () {
+    var file = fileInput.files[0];
+    reader = new FileReader();
+
+    reader.onload = function () {
+      document.getElementById("textarea1").value = reader.result;
+    };
+
+    reader.readAsText(file);
+  });
+
+  fileInput.click();
+}
+
+function saveFile() {}

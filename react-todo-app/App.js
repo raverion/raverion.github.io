b0b5af84f20e69dd429b7d1f0e81d795d576ef9f
Rej Averion <averionrej@gmail.com>
2023-02-24 09:14:27 +0400

    updated react component

--- react-todo-app/App.js
+++ react-todo-app/App.js
@@ -1,71 +1,26 @@
-import "./App.css";
-import { useState } from "react";
-import { useEffect } from "react";
+"use strict";
 
-const App = () => {
-  const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState("");
+const e = React.createElement;
 
-  useEffect(() => {
-    document.title = "Basic Todo App";
-  }, []);
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
 
-  const addTodo = () => {
-    if (todo !== "") {
-      setTodos([...todos, todo]);
-      setTodo("");
+  render() {
+    if (this.state.liked) {
+      return "You liked this.";
     }
-  };
 
-  const deleteTodo = (text) => {
-    const newTodos = todos.filter((todo) => {
-      return todo !== text;
-    });
-    setTodos(newTodos);
-  };
+    return e(
+      "button",
+      { onClick: () => this.setState({ liked: true }) },
+      "Like"
+    );
+  }
+}
 
-  return (
-    <div className="App">
-      <h1> todo list </h1>
-
-      <div className="input-wrapper">
-        <input
-          type="text"
-          name="todo"
-          value={todo}
-          placeholder="Create a new task"
-          onChange={(event) => {
-            setTodo(event.target.value);
-          }}
-        />
-        <button className="add-button" onClick={addTodo}>
-          Add
-        </button>
-      </div>
-
-      {todos?.length > 0 ? (
-        <ul className="todo-list">
-          {todos.map((todo, index) => (
-            <div className="todo">
-              <li key={index}> {todo} </li>
-              <button
-                className="delete-button"
-                onClick={() => {
-                  deleteTodo(todo);
-                }}
-              >
-                Delete
-              </button>
-            </div>
-          ))}
-        </ul>
-      ) : (
-        <div className="empty">
-          <p>No task found</p>{" "}
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default App;
+const domContainer = document.querySelector("#like_button_container");
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
--- react-todo-app/index.js
+++ react-todo-app/index.js
@@ -1,6 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+// import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+// import App from "./App";
+
+console.log("test message");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
